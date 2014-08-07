4c7484122ac6c4ae60292bba7669a07e4c0be111
TienNB <elvis1051020004@gmail.com>
2014-08-07 14:04:59 +0700

    change main act

--- TeamBGitTester/src/com/example/teambgittester/MainActivity.java
+++ TeamBGitTester/src/com/example/teambgittester/MainActivity.java
@@ -11,10 +11,5 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// testerkjshdfs
-		//Vu sua 
-		//aaaa
-		//ahskdha
-		//klaskld
 	}
 }
