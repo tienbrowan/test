f81091800d8a98781bf30031cdddd24b9273b8f0
vudv <vu.gemtek@gmail.com>
2014-08-07 15:04:34 -0800

    xoa het

--- TeamBGitTester/src/com/example/teambgittester/MainActivity.java
+++ TeamBGitTester/src/com/example/teambgittester/MainActivity.java
@@ -9,15 +9,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// xinchao
-
-		// kadhfa
-		// lahsfla
-		// them nhe1
-		// them2
-		// quen khong sua
-		// them3
-
-		alo xo
+	
 	}
 }
