18f5d554a0106fd0734ea1d2766a472cb5dba9ff
vudv <vu.gemtek@gmail.com>
2014-08-07 15:23:45 -0800

    lalalallalalla

--- TeamBGitTester/src/com/example/teambgittester/MainActivity.java
+++ TeamBGitTester/src/com/example/teambgittester/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	
+	//lalal
 		// xinchao
 
 		// kadhfa
